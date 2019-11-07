f4ce7bf9060c03ebf5b5f01bfb74a01843a7ae2c
hmb <humberto.munozb@gmail.com>
2019-11-06 17:39:15 -0800

    added nalgebra impl

---
+++ src/impls/nalgebra.rs
@@ -0,0 +1,33 @@
+use alga::general::{RealField, ClosedAdd, ClosedMul};
+use nalgebra::{Scalar,Dim, Matrix, DMatrix};
+use num_complex::Complex;
+use num_traits::Float;
+use std::ops::{Add, Mul};
+
+use crate::LinearCombination;
+use nalgebra::base::storage::StorageMut;
+
+impl<N, R, C, S, > LinearCombination<N> for Matrix<N, R, C, S>
+where N: Scalar + ClosedAdd + ClosedMul, R: Dim, C: Dim, S: StorageMut<N, R, C>
+
+{
+    fn scale(&mut self, k: N) {
+        *self *= k;
+    }
+
+    fn scalar_multiply_to(&self, k: N, target: &mut Self) {
+        for (s, t) in self.iter().zip(target.iter_mut()){
+            *t = *s * k;
+        }
+    }
+
+    fn add_scalar_mul(&mut self, k: N, other: &Self) {
+        for (s, t) in self.iter_mut().zip(other.iter()){
+            *s = k * *t + *s ;
+        }
+    }
+
+    fn add_assign_ref(&mut self, other: &Self) {
+        *self += other;
+    }
+}
