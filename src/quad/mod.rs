fab4990d69ebc8edba0bd95c8ae1b9f804c62b67
hmb <humberto.munozb@gmail.com>
2021-06-23 11:41:53 -0700

    Add quad module

---
+++ src/quad/mod.rs
@@ -0,0 +1,18 @@
+use crate::base::NormedLinearCombination;
+
+pub fn trapezoid(){
+
+}
+
+pub trait Quadrature{
+    
+    fn sum<T: Copy + Into<f64>, S: Copy, V, L: NormedLinearCombination<T, S, V> >(
+        &mut self,
+        x_arr: & [V],
+        lc: L,
+        res: &mut V
+    );
+}
+struct TrapezoidQuad{
+
+}
